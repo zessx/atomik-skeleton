1f1ab1a5c79aca5e3b155ede0b5f1f8e9a343322
zessx <contact@zes.sx>
2013-10-16 15:36:37 +0200

    Remove files when item is deleted

--- app/actions/clients/supprimer.php
+++ app/actions/clients/supprimer.php
@@ -6,6 +6,10 @@ if (!isset($this['request.id'])) {
 	
 } else {
 
+	$client = $this['db']->selectOne('clients', array('id_client' => $this['request.id']));
+	if($client['logo'] && !Uploader::delete($client['logo']))
+		$this->flash('Une erreur est survenue lors de la suppression du logo du client.', 'danger');
+
 	if($this['db']->delete('clients', array('id_client' => $this['request.id']))) {
 		Tools::log('clients', $this['request.id'], 'delete');
 		$this->flash('Le client a bien été supprimé.', 'success');
