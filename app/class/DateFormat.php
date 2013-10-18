d0be85195ce3dbcd6fa0fa3ded8b10caeb09704a
zessx <contact@zes.sx>
2013-10-18 12:13:18 +0200

    Fix issue when date is not defined

--- app/class/DateFormat.php
+++ app/class/DateFormat.php
@@ -2,17 +2,18 @@
 
 class DateFormat {
 
-	public static function alter($date, $before, $after) {
-		$dt = DateTime::createFromFormat($before, $date);
-		return $dt->format($after);
-	}
+    public static function alter($date, $before, $after) {
+        $dt = DateTime::createFromFormat($before, $date);
+        if($dt) return $dt->format($after);
+        return null;
+    }
 
-	public static function toSQL($date, $before = 'd/m/Y') {
-		return self::alter($date, $before, 'Y-m-d H:i:s');
-	}
+    public static function toSQL($date, $before = 'd/m/Y') {
+        return self::alter($date, $before, 'Y-m-d H:i:s');
+    }
 
-	public static function toHTML($date, $before = 'Y-m-d H:i:s') {
-		return self::alter($date, $before, 'd/m/Y');
-	}
-	
+    public static function toHTML($date, $before = 'Y-m-d H:i:s') {
+        return self::alter($date, $before, 'd/m/Y');
+    }
+    
 }
